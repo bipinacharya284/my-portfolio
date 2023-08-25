e9428e3ad55f8ee6d823d3b1913c7a4d573e8a04
bipinacharya <bipinacharya284@gmail.com>
2023-08-25 15:35:21 +0545

    About me task completed

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <nav class="bg-gray-800">
+  <!-- from-transparent -->
+  <nav class="bg-gradient-to-r from-black to-transparent z-20 sticky top-0">
     <div class="max-w-7xl px-4 sm:px-6 lg:px-8 transition-all ease-in-out delay-300">
       <div class="flex items-center justify-between h-1/2 py-6 transition-all ease-in-out delay-300">
         <div class="flex items-center">
-          <div class="flex-shrink-0 text-white">Logo</div>
+          <div class="flex-shrink-0 text-2xl text-white">Acharya Bipin</div>
         </div>
-        <div class="hidden md:flex space-x-4 absolute right-10">
-          <a href="#" class="text-gray-300 text-xl hover:text-white">About Me</a>
-          <a href="#" class="text-gray-300 text-xl hover:text-white">Portfolio</a>
-          <a href="#" class="text-gray-300 text-xl hover:text-white">Never Ending List </a>
+        <div class="hidden md:flex space-x-4 absolute right-10 ">
+          <a href="#about"
+            class="text-gray-400 text-xl transition ease-in-out duration-300 hover:text-white scroll-smooth">About
+            Me</a>
+          <a href="#" class="text-gray-400 text-xl transition ease-in-out duration-300 hover:text-white">Portfolio</a>
+          <a href="#" class="text-gray-400 text-xl transition ease-in-out duration-300 hover:text-white">Never Ending List
+          </a>
         </div>
         <div class="flex flex-col">
           <div class="md:hidden">
-            <button class="text-white" @click="toggleMobileMenu">
+            <!-- transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none ..." -->
+            <button class="text-white " @click="toggleMobileMenu">
               <Icon v-if="!isMobileMenuActivate" name="bi:list" />
               <Icon v-else name="bi:x-lg" />
             </button>
@@ -32,6 +37,7 @@
 </template>
 
 <script>
+// import smoothScroll from 'plugins/smooth-scroll';
 export default {
   data() {
     return {
@@ -45,3 +51,11 @@ export default {
   },
 };
 </script>
+
+
+<style>
+template {
+
+  scroll-behavior: smooth;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+      
     },
   },
   modules: ["nuxt-icon"],
